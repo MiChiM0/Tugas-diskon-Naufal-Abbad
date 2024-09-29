25ff1264115522299b22ffa1a84be2c4e134b9a7
M. Naufal Abbad Mu'afa Rizqullah <naufalabbad26@gmail.com>
2024-09-30 06:15:39 +0700

    Tugas Diskon.cpp

--- Tugas Diskon.cpp
+++ Tugas Diskon.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-int main(){
+int main() {
     int realPrice = 150000;
     double discount = 12.5;
-    int discountValue = realPrice * (discount/100);
+    int discountValue = realPrice * (discount / 100);
     int finalPrice = realPrice - discountValue;
 
-    std::cout << "Harga : " << realPrice << "ribu" << '\n';
+    std::cout << "Harga : " << realPrice << " ribu" << '\n';
     std::cout << "Diskon : " << discount << '\n';
-    std::cout << "Besarnya diskon dari harga : " << realPrice * (discount/100) << '\n';
-    std::cout << "Harga setelah diskon " << finalPrice << "ribu";
-    return 0;     
+    std::cout << "Besarnya diskon dari harga : " << realPrice * (discount / 100) << " ribu" << '\n';
+    std::cout << "Harga setelah diskon " << "Rp." << finalPrice << " ribu";
+    return 0;
 
 }
